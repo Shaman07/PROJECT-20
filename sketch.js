a213a1c18cd56d904462aa9c36264b9827e50fb3
Shaman07 <71549756+Shaman07@users.noreply.github.com>
2020-11-12 18:00:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,15 +8,16 @@ function setup() {
  
   speed = random(55,90);
   weight =random(400,1500);
-  deformation = speed*speed*weight*0.5/22500
-}
-
-function draw() {
+  
   car = createSprite(50,200,50,50);
   car.velocityX=speed/10;
   wall = createSprite(1500,200,60,400);
   wall.shapeColor=(800,80,80);
+}
 
+function draw() {
+ background(0);    
+deformation = speed*speed*weight*0.5/22500
   if(car.x-wall.x<car.width/2+wall.width/2
     &&wall.x-car.x<car.width/2+wall.width/2
     && car.y-wall.y<car.height/2+wall.height/2
@@ -34,6 +35,6 @@ function draw() {
       
       car.shapeColor = "red";
     }
-  background(0);  
+ 
   drawSprites();
 }
